2013b9887efb50cb87cc3087fd6e91f73a4e7b53
Txai Mostardeiro Potier <redtxai@gmail.com>
2015-11-15 19:04:14 -0200

    Adicionado botão na activity horários.

--- app/src/main/java/andreotxai/busaodadepressaoz/ActivityHorarios.java
+++ app/src/main/java/andreotxai/busaodadepressaoz/ActivityHorarios.java
@@ -1,30 +1,23 @@
 package andreotxai.busaodadepressaoz;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
 
-import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.text.InputType;
-import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class ActivityHorarios extends AppCompatActivity {
 
     private DatePickerDialog.OnDateSetListener dpickerListner = this.createDataListener();
-    private Button btn;
+    private Button btnData;
+    private Button btnProximo;
     private int year_x, month_x,day_x;
 
     public static final int DIALOG_ID = 0;
@@ -34,19 +27,21 @@ public class ActivityHorarios extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_horarios);
 
-        final Calendar cal = Calendar.getInstance();
+        this.btnProximo = (Button) findViewById(R.id.botaoAvancar);
+        this.btnProximo.setOnClickListener(this.createBotaoProximoClickListener());
 
+        final Calendar cal = Calendar.getInstance();
         this.year_x = cal.get(Calendar.YEAR);
         this.month_x = cal.get(Calendar.MONTH);
-        day_x = cal.get(Calendar.DAY_OF_MONTH);
+        this.day_x = cal.get(Calendar.DAY_OF_MONTH);
 
-        mostraDialogOnButtonClick();
+        this.mostraDialogOnButtonClick();
     }
 
     public void mostraDialogOnButtonClick(){
-        btn = (Button) findViewById(R.id.samuseucu);
+        this.btnData = (Button) findViewById(R.id.botaoData);
 
-        btn.setOnClickListener(
+        this.btnData.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -74,6 +69,18 @@ public class ActivityHorarios extends AppCompatActivity {
                 day_x = dayOfMonth;
                 Toast.makeText(ActivityHorarios.this,day_x + "/" + month_x + "/" + year_x, Toast.LENGTH_LONG).show();
             }
+        };
+    }
+
+    private View.OnClickListener createBotaoProximoClickListener() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Intent it = new Intent(ActivityHorarios.this, ActivityAvaliacao.class);
+                // startActivity(it);
+            }
+
         };
     }
 }
