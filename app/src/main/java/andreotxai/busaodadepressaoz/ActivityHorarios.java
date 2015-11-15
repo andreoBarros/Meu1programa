836cb71f6257ffd4675effe9afe2800645a5cfbb
Ândreo <andreo.dias.barros@gmail.com>
2015-11-15 18:14:27 -0200

    Adicionar Calendário

--- app/src/main/java/andreotxai/busaodadepressaoz/ActivityHorarios.java
+++ app/src/main/java/andreotxai/busaodadepressaoz/ActivityHorarios.java
@@ -1,30 +1,77 @@
 package andreotxai.busaodadepressaoz;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityHorarios extends AppCompatActivity {
+
+    Button btn;
+    int year_x, month_x,day_x;
+    static final int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_horarios);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final Calendar cal = Calendar.getInstance();
+
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        mostraDialogOnButtonClick();
     }
 
+    public void mostraDialogOnButtonClick(){
+        btn = (Button) findViewById(R.id.samuseucu);
+
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                    }
+                });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        if (id == DIALOG_ID){
+            return new DatePickerDialog(this, dpickerListner, year_x, month_x, day_x);
+        }
+        else{
+            return null;
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener dpickerListner
+            = new OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear + 1;
+            day_x = dayOfMonth;
+            Toast.makeText(ActivityHorarios.this,day_x + "/" + month_x + "/" + day_x, Toast.LENGTH_LONG).show();
+        }
+    };
 }
+
